cffece3cd6aae2c050ac1500e9740b02296eb72f
Akshay <akshay_111meher@hotmail.com>
2024-11-17 17:06:15 +0400

    update

---
+++ enclave/refresh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+nitro-cli terminate-enclave --all
+nitro-cli run-enclave --cpu-count 2 --memory 5000 --eif-path nitro-enclave.eif --enclave-cid 88 --debug-mode
+nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
