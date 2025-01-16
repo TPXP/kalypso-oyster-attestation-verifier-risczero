a62c8dfc6ee998a1d6e381e38813c984e50574ee
Akshay <akshay_111meher@hotmail.com>
2025-01-16 13:16:46 +0400

    Add Polling time Itnerrval

--- host/src/prover.rs
+++ host/src/prover.rs
@@ -55,6 +55,7 @@ async fn main() -> std::io::Result<()> {
                 max_parallel_proofs,
                 false,
                 9999,
+                0
             );
 
         listener.run().await
